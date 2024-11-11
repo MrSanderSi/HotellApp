da58808a4f73d6306e8064c89186879e26eadde9
Sander Sihver <sihversass@gmail.com>
2024-11-11 14:45:51 +0200

    controller conntection hotfix

--- hotellapp.client/src/app/app.component.ts
+++ hotellapp.client/src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit {
   }
 
   getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+    this.http.get<WeatherForecast[]>('/HotellBooking').subscribe(
       (result) => {
         this.forecasts = result;
       },
